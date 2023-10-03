c0dec09edacee4dbd300e1f270954af88c878e11
Владислав Иванов <hezangames@gmail.com>
2023-10-03 21:43:42 +0300

    fonts, textmeshpro, floor scene unity

--- Assets/Scripts/Building builder/BuildingBuilder.cs
+++ Assets/Scripts/Building builder/BuildingBuilder.cs
@@ -1,16 +1,56 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingBuilder : MonoBehaviour
 {
     [SerializeField] private Transform _floorPrefab;
     [SerializeField] private int _availableFloorsTest;
+    [SerializeField] private TextMeshProUGUI _availableFloorsText;
+    [SerializeField] private TextMeshProUGUI _availableFloorsHeaderText;
+    [SerializeField] private Button _placeFloor;
+    [SerializeField] private Button _loadResourcesScene;
+    [SerializeField] private Transform _virtualCameraTarget;
+    [SerializeField] private float _floorOffsetY = 3f;
 
     private int _availableFloors;
+    private List<Transform> _floors = new List<Transform>();
 
     private void Start()
     {
         _availableFloors = _availableFloorsTest;
+        _availableFloorsText.text = _availableFloors.ToString();
+
+        _placeFloor.onClick.AddListener(PlaceFloor);
+        _loadResourcesScene.onClick.AddListener(LoadResourcesScene);
     }
 
-    
+    private void PlaceFloor()
+    {
+        if (_availableFloors <= 0)
+        {
+            _availableFloorsHeaderText.gameObject.SetActive(false);
+            _availableFloorsText.gameObject.SetActive(false);
+            _placeFloor.gameObject.SetActive(false);
+            _loadResourcesScene.gameObject.SetActive(true);
+            return;
+        }
+
+        var prefabInstance = Instantiate(_floorPrefab, transform);
+
+        if (_floors.Count != 0)
+            prefabInstance.transform.position = _floors[_floors.Count - 1].position + Vector3.up * _floorOffsetY;
+
+        _virtualCameraTarget.position = prefabInstance.transform.position;
+        _floors.Add(prefabInstance);
+
+        _availableFloors--;
+        _availableFloorsText.text = _availableFloors.ToString();
+    }
+
+    private void LoadResourcesScene()
+    {
+
+    }
 }
