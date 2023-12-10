2e7dd6cecf8afcebfbaa0bce8d7560acfde99dbf
Denis Khabibullin <den.khabibullin@gmail.com>
2023-12-10 23:29:41 +0300

    add reward

--- Assets/_Project/Scripts/Logic/Idle Game/Model/FloorsProgressModel.cs
+++ Assets/_Project/Scripts/Logic/Idle Game/Model/FloorsProgressModel.cs
@@ -5,6 +5,7 @@ namespace Logic.Model
 {
     public class FloorsProgressModel
     {
-        public List<Vector3> setFloors;
+        public List<Vector3> setFloors; // поставленные этажи для сохранения
+        public int reward; // итоговая награда
     }
 }
