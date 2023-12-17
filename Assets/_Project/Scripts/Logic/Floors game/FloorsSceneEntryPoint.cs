a00c76afa8b01668cf8304a6f48ac4ebe570bc92
Владислав Иванов <hezangames@gmail.com>
2023-12-17 10:07:25 +0300

    floor entry change

--- Assets/_Project/Scripts/Logic/Floors game/FloorsSceneEntryPoint.cs
+++ Assets/_Project/Scripts/Logic/Floors game/FloorsSceneEntryPoint.cs
@@ -12,18 +12,15 @@ public class FloorsSceneEntryPoint : MonoBehaviour
     [SerializeField] private bool _debug = false;
     [SerializeField] private FloorsContextView _floorsContextView;
     [SerializeField] private FloorsSceneSettings _settings;
-    [SerializeField] private EntryPoint.ResourceLoadType _resourceLoadType;
 
     private FloorsScenePm _root;
-    private IResourceLoader _resourceLoader;
 
     private void Awake()
     {
         if (_debug)
             _root = new FloorsScenePm(new FloorsScenePm.Ctx
             {
-                sceneContext = _floorsContextView,
-                resourceLoader = _resourceLoader                
+                sceneContext = _floorsContextView             
             });
     }
 }
