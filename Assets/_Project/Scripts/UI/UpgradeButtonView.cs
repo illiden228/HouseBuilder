2389824cb7eb017696eb01f28e40a38a407e77a8
Denis Khabibullin <den.khabibullin@gmail.com>
2023-12-24 12:37:13 +0300

    add upgrade buttons

---
+++ Assets/_Project/Scripts/UI/UpgradeButtonView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Core;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UpgradeButtonView : BaseMonobehaviour
+    {
+        [SerializeField] private Button _upgradeButton;
+        [SerializeField] private TMP_Text _currentLevelText;
+        [SerializeField] private TMP_Text _currentCost;
+        
+        public struct Ctx
+        {
+            public CompositeDisposable viewDisposable;
+            public IReadOnlyReactiveProperty<int> cost;
+            public IReadOnlyReactiveProperty<int> currentLevel;
+            public Action upgradeClick;
+        }
+
+        private Ctx _ctx;
+
+        public void Init(Ctx ctx)
+        {
+            _ctx = ctx;
+
+            _upgradeButton.OnClickAsObservable().Subscribe(_ => _ctx.upgradeClick?.Invoke()).AddTo(_ctx.viewDisposable);
+            _ctx.currentLevel.Subscribe(value => _currentLevelText.text = value.ToString()).AddTo(_ctx.viewDisposable);
+            _ctx.cost.Subscribe(value => _currentCost.text = value.ToString()).AddTo(_ctx.viewDisposable);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/UI/UpgradeButtonsView.cs
@@ -0,0 +1,34 @@
+﻿using Core;
+using UnityEngine;
+
+namespace UI
+{
+    public class UpgradeButtonsView : BaseMonobehaviour
+    {
+        [SerializeField] private UpgradeButtonView _effectiencyUpButton;
+        [SerializeField] private UpgradeButtonView _timeSpeedUpButton;
+        [SerializeField] private UpgradeButtonView _addWorkerButton;
+        [SerializeField] private UpgradeButtonView _mergeButton;
+        
+        public struct Ctx
+        {
+            
+        }
+
+        private Ctx _ctx;
+
+        public UpgradeButtonView EffectiencyUpButton => _effectiencyUpButton;
+
+        public UpgradeButtonView TimeSpeedUpButton => _timeSpeedUpButton;
+
+        public UpgradeButtonView AddWorkerButton => _addWorkerButton;
+
+        public UpgradeButtonView MergeButton => _mergeButton;
+
+        public void Init(Ctx ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
+
